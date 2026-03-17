c56574c308a50e6f7aa4f37fbcca221b2d10a441
Timur <Snurikstupenko@gmail.com>
2026-03-17 10:25:35 +0200

    update version

---
+++ цй.cpp
@@ -0,0 +1,5 @@
+//
+// Created by husus on 17.03.2026.
+//
+
+#include "цй.h"
---
+++ цй.h
@@ -0,0 +1,14 @@
+//
+// Created by husus on 17.03.2026.
+//
+
+#ifndef TELEGRAM_ЦЙ_H
+#define TELEGRAM_ЦЙ_H
+
+
+class цй
+{
+};
+
+
+#endif //TELEGRAM_ЦЙ_H
